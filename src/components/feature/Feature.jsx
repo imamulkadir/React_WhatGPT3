e31b1b265930073c1c1411b266016c449ccbc649
imamulkadir <imamulkadiroyan@gmail.com>
2025-08-01 04:11:14 +0600

    Initial Commit

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,7 @@
+import "./feature.css";
+
+const Feature = () => {
+  return <div>1:48:22</div>;
+};
+
+export default Feature;
---
+++ src/container/footer/Footer.jsx
@@ -0,0 +1,7 @@
+import "./footers.css";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
